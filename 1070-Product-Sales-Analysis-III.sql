3480b05625626cce2305f12e3cb47a0b56b0b424
Thanush Naidu <thanushnaidu.bodapati@gmail.com>
2026-08-10 23:48:52 +0530

    [1070] [Time Beats: 16.12%] - LeetPush

---
+++ 1070-Product-Sales-Analysis-III.sql
@@ -0,0 +1,2 @@
+# Write your MySQL query statement below
+select product_id,year as first_year,quantity,price from(select sale_id,product_id,year,quantity,price,dense_rank() over(partition by product_id order by year asc) as rnk from sales)t where rnk = 1;
